b840d43233f8fe1b2fdbe6696b0d607d9fd5238b
Hanabi <nlime3141592@naver.com>
2022-08-07 00:17:47 +0900

    Add Unity Dependenets module.

--- JlMetroidvaniaProject/UnityDependents/_Classes/GameKey.cs
+++ JlMetroidvaniaProject/UnityDependents/_Classes/GameKey.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-namespace JlMetroidvaniaProject
+namespace JlMetroidvaniaProject.UnityDependents
 {
     public class GameKey<T_GameKeyEnum>
     where T_GameKeyEnum : System.Enum
---
+++ JlMetroidvaniaProject/UnityDependents/_Extensions/JsonUtilityExtension_Skills.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+namespace JlMetroidvaniaProject.UnityDependents
+{
+    public static partial class JsonUtilityExtension
+    {
+        public static string ToJson(this SkillMetadata data, bool prettyPrint = false)
+        {
+            return JsonUtility.ToJson(data, prettyPrint);
+        }
+
+        public static string ToJson(this SkillTree tree, bool prettyPrint = false)
+        {
+            return JsonUtility.ToJson(tree, prettyPrint);
+        }
+
+        public static string ToJson(this SkillChart chart, bool prettyPrint = false)
+        {
+            return JsonUtility.ToJson(chart, prettyPrint);
+        }
+
+        public static string ToJson(this SkillSet set, bool prettyPrint = false)
+        {
+            return JsonUtility.ToJson(set, prettyPrint);
+        }
+
+        public static void FromJsonOverwrite(this SkillMetadata data, string json)
+        {
+            JsonUtility.FromJsonOverwrite(json, data);
+        }
+
+        public static void FromJsonOverwrite(this SkillTree tree, string json)
+        {
+            JsonUtility.FromJsonOverwrite(json, tree);
+        }
+
+        public static void FromJsonOverwrite(this SkillChart chart, string json)
+        {
+            JsonUtility.FromJsonOverwrite(json, chart);
+        }
+
+        public static void FromJsonOverwrite(this SkillSet set, string json)
+        {
+            JsonUtility.FromJsonOverwrite(json, set);
+        }
+    }
+}
